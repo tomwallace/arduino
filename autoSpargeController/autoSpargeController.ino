9c38f89cb258480004f48a626cf7a89de48f1df6
Tom Wallace <thomaspwallace@gmail.com>
2017-09-08 12:12:35 -0400

    Adding a simple test mode to support John's circuit board making process.

--- autoSpargeController/autoSpargeController.ino
+++ autoSpargeController/autoSpargeController.ino
@@ -1,16 +1,29 @@
 /*
  * AUTOSPARGE CONTROLLER
- * version 0.4
+ * version 0.5
  * by Tom Wallace and John Baker
  * This sketch controls the components hooked up to the Auto Sparge assembly, which controls the rate 
  * of sparge water going into the mash tun and the rate of wort leaving the mash tun.  It is designed to
  * work with a standard brewery build from The Electric Brewery.  The controller interacts with two pumps, 
  * and two probes.
  */
-// TODO: Consider making an ALARM class
+// Define Pins
+#define TRINKET_BOARD_LED_PIN 13
 #define ALARM 8
+#define BUZZER 11
+#define LEFT_BUTTON_PIN 4
+#define LEFT_BUTTON_LIGHT_PIN 6
+#define RIGHT_BUTTON_PIN 3
+#define RIGHT_BUTTON_LIGHT_PIN 5
+#define MASH_PROBE_PIN 16
+#define MASH_PROBE_HIGH_PIN 17
+#define BOIL_PROBE_PIN 15
+#define WATER_PUMP_PIN 13
+#define WORT_PUMP_PIN 12
+
 #define ALARM_SOUND HIGH
 #define ALARM_SILENT LOW
+
 #define MAINT_MODE_BEEP_INTERVAL 4950
 #define MAINT_MODE_BEEP_LENGTH 50
 
@@ -216,7 +229,7 @@ class WaterPump : Loggable {
   }
 };
 
-class Button : public Loggable {
+class Button : Loggable {
   int BUTTON_BEEP_LENGTH = 10;  // Length of beep when button pressed
   int HAS_BEEN_CLICKED_DELAY = 500; // Length of delay before button is eligible for clicking again (prevents burst by holding button down)
 
@@ -248,6 +261,10 @@ class Button : public Loggable {
 
   bool IsClickedWithDelay() {
     return (HasBeenClicked && (! EligibleToBeClicked));
+  }
+
+  bool IsCurrentlyDepressed() {
+    return (digitalRead(ButtonPin) == LOW);
   }
 
   bool GetMatchingFunctionOn() {
@@ -349,26 +366,29 @@ class MaintenanceMode {
 };
 
 // Create objects
-Button LeftButton("Left Button", 4, INPUT_PULLUP, 6);
-Button RightButton("Right Button", 3, INPUT_PULLUP, 5);
+Button LeftButton("Left Button", LEFT_BUTTON_PIN, INPUT_PULLUP, LEFT_BUTTON_LIGHT_PIN);
+Button RightButton("Right Button", RIGHT_BUTTON_PIN, INPUT_PULLUP, RIGHT_BUTTON_LIGHT_PIN);
 
-Probe MashProbe("Mash Probe", 16, INPUT);
-Probe MashProbeHigh("Mash Probe High", 17, INPUT);
-Probe BoilProbe("Boil Probe", 15, INPUT);
+Probe MashProbe("Mash Probe", MASH_PROBE_PIN, INPUT);
+Probe MashProbeHigh("Mash Probe High", MASH_PROBE_HIGH_PIN, INPUT);
+Probe BoilProbe("Boil Probe", BOIL_PROBE_PIN, INPUT);
 
-WaterPump WaterPump(13, 10000);
-WortPump WortPump(12, 2000);  // TODO: Pull this value from writeable memory
+WaterPump WaterPump(WATER_PUMP_PIN, 10000);
+WortPump WortPump(WORT_PUMP_PIN, 2000);
 
 MaintenanceMode MaintMode(LeftButton.GetButtonPin(), RightButton.GetButtonPin());
 
-
+bool FirstTimeThroughCode = true;
+bool InTestMode = false;
 
 long MaintModeBeepMillis = 0;
 int BuzzerState = ALARM_SILENT;
 
 // put your setup code here, to run once:
 void setup() {
+  pinMode(TRINKET_BOARD_LED_PIN, OUTPUT);
   pinMode(ALARM, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 
   // Set up serial port for output at 9600 bps
   Serial.begin(9600);
@@ -378,6 +398,53 @@ void setup() {
 void loop() {
   // Get current clock
   unsigned long currentMillis = millis();
+
+  // Will only be run in first millisecond of code - used for Test mode evaluation
+  if (FirstTimeThroughCode) {
+    FirstTimeThroughCode = false;
+    // Puts us in Test mode - only way to get out is to cycle power or reset and LeftButton is NOT pushed
+    if (LeftButton.IsCurrentlyDepressed()) {
+      Serial.println(String(currentMillis) + " - Start-up: Entering test mode");
+      InTestMode = true;
+
+      // Flash buttons
+      int counter = 0;
+      while(counter < 3) {
+        digitalWrite(LEFT_BUTTON_LIGHT_PIN, HIGH);
+        digitalWrite(RIGHT_BUTTON_LIGHT_PIN, HIGH);
+        digitalWrite(TRINKET_BOARD_LED_PIN, HIGH);
+        delay(250);
+        digitalWrite(LEFT_BUTTON_LIGHT_PIN, LOW);
+        digitalWrite(RIGHT_BUTTON_LIGHT_PIN, LOW);
+        digitalWrite(TRINKET_BOARD_LED_PIN, LOW);
+        delay(250);
+        counter++;
+      }
+    }
+  }
+
+  // Test mode routine - only way to get out is to cycle power or reset and LeftButton is NOT pushed
+  if (InTestMode) {
+    if (LeftButton.IsCurrentlyDepressed()) {
+      digitalWrite(LEFT_BUTTON_LIGHT_PIN, HIGH);
+      digitalWrite(WATER_PUMP_PIN, HIGH);
+      digitalWrite(BUZZER, HIGH);
+    } else {
+      digitalWrite(LEFT_BUTTON_LIGHT_PIN, LOW);
+      digitalWrite(WATER_PUMP_PIN, LOW);
+      digitalWrite(BUZZER, LOW);
+    }
+
+    if (RightButton.IsCurrentlyDepressed()) {
+      digitalWrite(RIGHT_BUTTON_LIGHT_PIN, HIGH);
+      digitalWrite(WORT_PUMP_PIN, HIGH);
+      digitalWrite(ALARM, HIGH);
+    } else {
+      digitalWrite(RIGHT_BUTTON_LIGHT_PIN, LOW);
+      digitalWrite(WORT_PUMP_PIN, LOW);
+      digitalWrite(ALARM, LOW);
+    }
+  }
 
   // TODO: Return to maintenance mode after finished with base functionality
     /*
@@ -418,8 +485,8 @@ void loop() {
 */
 
   // Update objects
-  LeftButton.Update(currentMillis, ALARM);
-  RightButton.Update(currentMillis, ALARM);
+  LeftButton.Update(currentMillis, BUZZER);
+  RightButton.Update(currentMillis, BUZZER);
 
   // Determine if in maintenance mode, which trumps all other functions
   if (MaintMode.GetInMode()) {
